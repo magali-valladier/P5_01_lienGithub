199316104e66459b4fd8162e2e2a638bf9d8bb07
magali <digimaglink@gmail.com>
2021-02-07 15:56:34 +0100

    add sass css

--- index.js
+++ index.js
@@ -5,22 +5,28 @@ for (let i = 0; i < 5; i++) {
     cardDiv.classList.add("card");
     let div = document.getElementById("card");
     div.appendChild(cardDiv);
-
-// INSERTION IMG DANS CARD BODY
-
-    let imgBody = document.createElement("img");
-    cardDiv.appendChild(imgBody);
-    
-
 }
 
+// INSERTION IMG DANS CARD BODY
+    function Ours(colors,id,name,price,img) {
+        this.colors = colors;
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.img = img;
+    }
+    const ours1 = new Ours(["Tan","Chocolate","Black","White"], "_id:5be9c8541c9d440000665243", "Norbert", 2900, "http://localhost:3000/images/teddy_1.jpg");
+let ours = document.getElementsByClassName("card");
+
+
+
+
 // API REQUEST
-let bears;
 
-const fetchTeddies = async() => {
 
-bears = await fetch("http://localhost:3000/api/teddies")
-.then(response => response.json());
-console.log(bears);
-};
-fetchTeddies();
+fetch("http://localhost:3000/api/teddies")
+.then(response => response.json())
+.then(data => {
+    console.log(data) 
+})
+.catch(error => console.error(error))
